99451f02ae92ae2a1c9bda81097958134602a63a
nchore27 <nchorefrankley27@gmail.com>
2023-01-25 02:22:17 +0000

    final commit

--- api/vi/app.py
+++ api/vi/app.py
@@ -1,38 +1,31 @@
 #!/usr/bin/python3
-""" Flask app """
-
+"""
+Module to register blueprints and run the flask server
+in preparation for api calls
+"""
 from api.v1.views import app_views
-from flask import Flask, jsonify, Blueprint, make_response
-from flask_cors import CORS
+from flask import Flask, jsonify
+from models import engine
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
-
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """ Teardown method """
+def teardown_flask(exception):
+    """
+    Remove the database, exit and save file
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def hande_404_error(error):
-    """ handles the 404 error
-        returns 404 json
-    """
-    data = {"error": "Not found"}
-
-    return make_response(jsonify(data), 404)
-
+def page_not_found(error):
+    return (jsonify(error="Not found"), 404)
 
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", "5000")))
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port)
